0ce90cd229f4e0ca01d0fdfd450638ab9c2759b1
Tsalyk <zalukm1@gmail.com>
2022-06-21 14:48:00 +0300

    outfit classes

---
+++ outfits.py
@@ -0,0 +1,6 @@
+OUTFITS = ['black_dress', 'black_pants', 'black_shirt', 'black_shoes',
+            'black_shorts', 'blue_dress', 'blue_pants', 'blue_shirt',
+            'blue_shoes', 'blue_shorts', 'brown_pants', 'brown_shoes',
+            'brown_shorts', 'green_pants', 'green_shirt', 'green_shoes',
+            'green_shorts', 'red_dress', 'red_pants', 'red_shoes', 'white_dress',
+            'white_pants', 'white_shoes', 'white_shorts']
